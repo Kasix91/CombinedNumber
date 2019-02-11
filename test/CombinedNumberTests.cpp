a9033199fe5edce2668ed65871b1337359539160
Katarzyna Chroma <katarzyna.chroma@gmail.com>
2019-02-11 17:40:45 +0100

    2 files updated

--- test/CombinedNumberTests.cpp
+++ test/CombinedNumberTests.cpp
@@ -1,11 +1,17 @@
 #include <gtest/gtest.h>
+#include <list>
 #include "CombinedNumber.hpp"
 
 struct CombinedNumberTests : public ::testing::Test
 {
 };
 
-TEST_F(CombinedNumberTests, assertThatFalseIsTrue)
+
+
+TEST_F(CombinedNumberTests, when1IsGivenStringIsReturned)
 {
-    ASSERT_TRUE(false);
+
+    CombinedNumber cn;
+    auto result = cn.constructGreatestNumber({1});
+    ASSERT_EQ(result, "1");
 }
